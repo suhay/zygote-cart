da4e3c2149b88e80398ce59cde2deea122e79cc0
Christian Sedlacek <csedlacek@escaladesports.com>
2018-01-03 08:43:51 -0600

    Add next-step-button component

---
+++ src/components/next-step-button.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default class extends React.Component {
+	render() {
+		return (
+			<button className="zygoteNextButton">
+				Next step
+				<style jsx global>{`
+					.zygoteNextButton {
+						display: inline-block;
+						border: 0;
+						outline: 0;
+						padding: 13px 20px;
+						min-width: 130px;
+						text-align: center;
+						text-transform: uppercase;
+						font-size: 0.8em;
+						font-weight: 700;
+						color: #fff;
+						background: #000;
+						cursor: pointer;
+						user-select: none;
+					}
+				`}</style>
+			</button>
+		)
+	}
+}
