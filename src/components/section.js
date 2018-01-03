05d6e19257ea35a0244ef704403c0df24f97bf75
Christian Sedlacek <csedlacek@escaladesports.com>
2018-01-03 11:42:22 -0600

    Responsively style section component

--- src/components/section.js
+++ src/components/section.js
@@ -3,18 +3,27 @@ import settings from './_settings'
 
 export default class extends React.Component {
 	render() {
-		const width = (parseInt(this.props.rowLength) ? (100/parseInt(this.props.rowLength)) : (100/3)) + '%';
 		return (
-			<div className={"zygoteSection" + (this.props.isLast ? ' zygoteSectionLast' : '')} style={{width}}>
+			<div className={"zygoteSection" + (this.props.isLast ? ' zygoteSectionLast' : '')}>
 				{this.props.children}
 				<style jsx global>{`
 					.zygoteSection {
-						display: table-cell;
-						border-right: 1px solid ${settings.gray};
+						display: block;
 						padding: 20px;
+						width: 100%;
 					}
-					.zygoteSection.zygoteSectionLast {
-						border-right: 0;
+					@media (min-width: ${settings.breakpoint}px) {
+						.zygoteSection {
+							display: table-cell;
+							border-right: 1px solid ${settings.gray};
+							width: 33.333%; /* fallback */
+							width: calc(1/3);
+						}
+
+						.zygoteSection.zygoteSectionLast {
+							border-right: 0;
+						}
+
 					}
 				`}</style>
 			</div>
