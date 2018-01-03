e38db8c9bb0226b3524e00b7b77f0407f126a2c5
Christian Sedlacek <csedlacek@escaladesports.com>
2018-01-03 10:11:58 -0600

    Add input component

---
+++ src/components/input.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+export default class extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleChange = this.handleChange.bind(this);
+	}
+	handleChange(e) {
+		this.props.handleChange(e.target.value);
+	}
+	render() {
+		return (
+			<label className="zygoteInput">
+				{this.props.label}
+				<input className="zygoteInputField" type={this.props.type || 'text'} name={this.props.name} maxLength={this.props.maxLength} onChange={this.handleChange} value={this.props.value} />
+				<style jsx global>{`
+					.zygoteInput {
+						display: block;
+						text-transform: uppercase;
+						font-size: 0.8em;
+						color: #000;
+						cursor: pointer;
+						user-select: none;
+					}
+
+					.zygoteInputField {
+						box-sizing: border-box;
+						border: 1px solid #ccc;
+						outline: none;
+						margin-top: 3px;
+						padding: 10px;
+						width: 100%;
+						height: 40px;
+						font-size: 1em;
+					}
+				`}</style>
+			</label>
+		)
+	}
+}
