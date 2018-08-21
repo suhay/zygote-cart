e30cd2273f2f7101d9c52a877ba54c2bc3ec1e6a
Kennedy Rose <kennedyianrose@gmail.com>
2018-08-21 16:20:29 -0500

    Fixed totals modification

--- src/export/utils/add-total-modification.js
+++ src/export/utils/add-total-modification.js
@@ -1,11 +1,39 @@
 import totalsState from '../state/totals'
-import productsState from '../state/products'
 
-export default function calculateSubtotal() {
-	const { products } = productsState.state
-	let subtotal = 0
-	products.forEach(({ quantity, price }) => {
-		subtotal += quantity * price
-	})
-	totalsState.setState({ subtotal })
+export default function addModification(newModification) {
+	/*
+	{
+		id: `10OFF`,
+		description: `10% off discount`,
+		displayValue: `-10%`,
+		alteration: () => {
+			return totalsState.state.subtotal * -.1
+		}
+	}
+	*/
+	if (Array.isArray(newModification)) {
+		totalsState.setState({
+			modifications: newModification,
+		})
+	}
+	else {
+		const modifications = [...totalsState.state.modifications]
+		// See if already exists
+		let modificationExists = false
+		if (newModification.id) {
+			for (let i = modifications.length; i--;) {
+				let mod = modifications[i]
+				if (mod.id && newModification.id === mod.id) {
+					modificationExists = true
+					modifications[i] = newModification
+				}
+			}
+		}
+		// Add new modification
+		if (!modificationExists) {
+			modifications.push(newModification)
+		}
+		totalsState.setState({ modifications })
+	}
+	totalsState.calculateTotal()
 }
