5f4687b3fbfe9de60dd87b69b56cd02365f5d40c
Kennedy Rose <kennedyianrose@gmail.com>
2018-10-09 14:50:39 -0500

    Validate card number

--- src/export/components/inputs/credit-card.js
+++ src/export/components/inputs/credit-card.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import { Number } from 'react-credit-card-primitives'
+import Card from 'creditcards/card'
+import types from 'creditcards-types'
 import Input from './input'
+
+console.log(types)
+
+const card = Card(types)
 
 export default class CreditCard extends React.Component {
 	static defaultProps = {
@@ -9,8 +15,9 @@ export default class CreditCard extends React.Component {
 		required: true,
 		name: `billingCardNumber`,
 	}
-	validate(valid){
-		if(!valid){
+	validate(val){
+		val = val.replace(/\D/g, ``)
+		if (!card.isValid(val)){
 			return `Please supply a valid credit card number`
 		}
 	}
@@ -24,7 +31,9 @@ export default class CreditCard extends React.Component {
 		} = this.props
 		return (
 			<Number
-				onChange={({ valid }) => this.validate(valid)}
+				onChange={({ type }) => {
+					console.log(type)
+				}}
 				render={({ getInputProps }) => {
 					const props = getInputProps()
 					delete props.placeholder
