accff264145aaea32c2406510ef2be5da3cfb9cd
Kennedy Rose <kennedyianrose@gmail.com>
2018-10-10 16:22:07 -0500

    Editable controls

--- src/export/index.js
+++ src/export/index.js
@@ -1,17 +1,8 @@
-import Cart from './components'
-import CartQuantity from './components/cart-quantity'
-import openCart from './utils/open-cart'
-import closeCart from './utils/close-cart'
-import toggleCart from './utils/toggle-cart'
-import addToCart from './utils/add-to-cart'
-import removeFromCart from './utils/remove-from-cart'
-
-export {
-	Cart,
-	CartQuantity,
-	openCart,
-	closeCart,
-	toggleCart,
-	addToCart,
-	removeFromCart,
-}
+export { default as Cart } from './components'
+export { default as CartQuantity } from './components/cart-quantity'
+export { default as openCart } from './utils/open-cart'
+export { default as closeCart } from './utils/close-cart'
+export { default as toggleCart } from './utils/toggle-cart'
+export { default as addToCart } from './utils/add-to-cart'
+export { default as removeFromCart } from './utils/remove-from-cart'
+export { default as settingsState } from './state/settings'
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import { css } from 'emotion'
-import { Cart, openCart, addToCart } from '../export'
+import { Cart, openCart, addToCart, settingsState } from '../export'
 import logo from '../img/logo.svg'
 
 export default class HomePage extends React.Component {
+	constructor(props){
+		super(props)
+		this.state = {
+			//stripeApiKey: `pk_test_0EMVTB6nEzmrjGA0Fc0kyVOR`,
+			paypalAppId: `ATP-SVtvHjAfyOGdr_8RRXgizsofojJV32mMt3WRmf5ignVi1TZkA67UYwm5sAitwETQuEigH91w70_6`,
+			paypalEnv: `sandbox`,
+			infoWebhook: `/.netlify/functions/info`,
+			orderWebhook: `/.netlify/functions/order`,
+		}
+	}
 	render() {
 		let products = this.props.data.allStripeSku.edges.map(edge => edge.node)
 		return (
@@ -36,17 +46,39 @@ export default class HomePage extends React.Component {
 					})}>Add to Cart B</button>
 				</div>
 
+				<div className={styles.editControls}>
+					{(() => {
+						const els = []
+						for(let i in this.state){
+							const changeHandler = ({ target: { value }}) => {
+								console.log(value)
+								settingsState.setState({
+									[i]: value,
+								})
+							}
+							els.push(
+								<div key={`control-${i}`}>
+									<label>
+										<span>{i}:</span>
+										<input
+											type='text'
+											value={this.state[i]}
+											onChange={changeHandler.bind(this)}
+										/>
+									</label>
+								</div>
+							)
+						}
+						return els
+					})()}
+				</div>
 
 				<Cart
-					header={<img className={logoStyles} src={logo} />}
-					cartHeader={<div className={headerStyles}>With FREE shipping!</div>}
-					cartFooter={<div className={footerStyles}>* Free shipping, except Alaska and Hawaii</div>}
+					header={<img className={styles.logo} src={logo} />}
+					cartHeader={<div className={styles.header}>With FREE shipping!</div>}
+					cartFooter={<div className={styles.footer}>* Free shipping, except Alaska and Hawaii</div>}
 
-					// stripeApiKey='pk_test_0EMVTB6nEzmrjGA0Fc0kyVOR'
-					paypalAppId='ATP-SVtvHjAfyOGdr_8RRXgizsofojJV32mMt3WRmf5ignVi1TZkA67UYwm5sAitwETQuEigH91w70_6'
-					paypalEnv='sandbox'
-					infoWebhook='/.netlify/functions/info'
-					orderWebhook='/.netlify/functions/order'
+					{...this.state}
 
 					totalModifications={[
 						{
@@ -68,21 +100,37 @@ export default class HomePage extends React.Component {
 	}
 }
 
-const logoStyles = css({
-	maxWidth: 150,
-	margin: `0 30px`,
-})
-
-const headerStyles = css({
-	textAlign: `center`,
-	fontWeight: `bold`,
-	fontSize: `1.3em`,
-})
-const footerStyles = css({
-	marginTop: 30,
-	textAlign: `center`,
-	fontSize: `.9em`,
-})
+const styles = {
+	logo: css`
+		max-width: 150px;
+		margin: 0 30px;
+	`,
+	header: css`
+		text-align: center;
+		font-weight: bold;
+		font-size: 1.3em;
+	`,
+	footer: css`
+		margin-top: 30px;
+		text-align: center;
+		font-size: .9em;
+	`,
+	editControls: css`
+		margin-top: 30px;
+		div{
+			margin-top: 5px;
+		}
+		span{
+			display: inline-block;
+			text-align: right;
+			width: 150px;
+			margin-right: 10px;
+		}
+		input{
+			width: 300px;
+		}
+	`,
+}
 
 export const query = graphql`
 	query HomePage {
